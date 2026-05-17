a94ad4e6044f7cc4504a2bb1e9b29ba615aa20c9
GulnazNurseit <gulnaznurseit1227@gmail.com>
2026-05-17 13:17:26 +0500

    Added pet UI, age system and animations

--- PetCare/Assets/Scripts/Pet/PetManager.cs
+++ PetCare/Assets/Scripts/Pet/PetManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PetManager : MonoBehaviour
 {
@@ -9,19 +10,34 @@ public class PetManager : MonoBehaviour
     public Slider happinessSlider;
     public Slider cleanlinessSlider;
 
-    public TMP_Text scoreText;
+    public Image hungerFill;
+    public Image happinessFill;
+    public Image cleanlinessFill;
 
+    public Image ageCircleFill;
+    public TMP_Text ageText;
+
+    public TMP_Text scoreText;
     public SpriteRenderer petRenderer;
 
     public Sprite normalSprite;
     public Sprite hungrySprite;
     public Sprite dirtySprite;
+    public Sprite happySprite;
+    public Sprite playSprite;
+
+    public Vector3 petFixedPosition = new Vector3(-1.3f, -2.2f, 0f);
 
     private float hunger = 100f;
     private float happiness = 100f;
     private float cleanliness = 100f;
 
     private int score = 0;
+    private int age = 1;
+    private float ageProgress = 0f;
+    private float maxAgeProgress = 100f;
+
+    private bool showingTemporaryEmotion = false;
 
     void Start()
     {
@@ -29,6 +45,7 @@ public class PetManager : MonoBehaviour
         SetupSlider(happinessSlider);
         SetupSlider(cleanlinessSlider);
 
+        FixPetTransform();
         UpdateUI();
         UpdatePetEmotion();
     }
@@ -43,28 +60,49 @@ public class PetManager : MonoBehaviour
         cleanliness = Mathf.Clamp(cleanliness, 0, 100);
         happiness = Mathf.Clamp(happiness, 0, 100);
 
+        FixPetTransform();
         UpdateUI();
-        UpdatePetEmotion();
+
+        if (!showingTemporaryEmotion)
+        {
+            UpdatePetEmotion();
+        }
     }
 
     public void FeedPet()
     {
-        hunger = Mathf.Clamp(hunger + 30f, 0, 100);
+        hunger = Mathf.Clamp(hunger + 35f, 0, 100);
+        happiness = Mathf.Clamp(happiness + 5f, 0, 100);
         score += 10;
-        UpdateUI();
-        UpdatePetEmotion();
+        AddAgeProgress(10f);
+
+        StartCoroutine(ShowEmotionForSeconds(happySprite));
     }
 
     public void CleanPet()
     {
-        cleanliness = Mathf.Clamp(cleanliness + 30f, 0, 100);
+        cleanliness = Mathf.Clamp(cleanliness + 35f, 0, 100);
+        happiness = Mathf.Clamp(happiness + 5f, 0, 100);
         score += 10;
-        UpdateUI();
-        UpdatePetEmotion();
+        AddAgeProgress(10f);
+
+        StartCoroutine(ShowEmotionForSeconds(happySprite));
     }
 
     public void PlayMiniGame()
     {
+        StartCoroutine(ShowPlayThenLoad());
+    }
+
+    private IEnumerator ShowPlayThenLoad()
+    {
+        showingTemporaryEmotion = true;
+
+        petRenderer.sprite = playSprite;
+        petRenderer.transform.localScale = new Vector3(0.4f, 0.4f, 1f);
+
+        yield return new WaitForSeconds(0.7f);
+
         SceneManager.LoadScene("Garden_Level2");
     }
 
@@ -75,13 +113,55 @@ public class PetManager : MonoBehaviour
         slider.interactable = false;
     }
 
+    private void FixPetTransform()
+    {
+        petRenderer.transform.position = petFixedPosition;
+        petRenderer.sortingOrder = 10;
+    }
+
     private void UpdateUI()
     {
         hungerSlider.value = hunger;
         happinessSlider.value = happiness;
         cleanlinessSlider.value = cleanliness;
 
+        hungerFill.color = GetSliderColor(hunger);
+        happinessFill.color = GetSliderColor(happiness);
+        cleanlinessFill.color = GetSliderColor(cleanliness);
+
         scoreText.text = "Score: " + score;
+
+        ageCircleFill.fillAmount = ageProgress / maxAgeProgress;
+        ageText.text = "Age " + age;
+    }
+
+    private void AddAgeProgress(float amount)
+    {
+        ageProgress += amount;
+
+        if (ageProgress >= maxAgeProgress)
+        {
+            age++;
+            ageProgress = 0f;
+        }
+
+        UpdateUI();
+    }
+
+    private Color GetSliderColor(float value)
+    {
+        if (value > 70)
+        {
+            return Color.green;
+        }
+        else if (value > 50)
+        {
+            return Color.yellow;
+        }
+        else
+        {
+            return Color.red;
+        }
     }
 
     private void UpdatePetEmotion()
@@ -89,14 +169,30 @@ public class PetManager : MonoBehaviour
         if (hunger < 50)
         {
             petRenderer.sprite = hungrySprite;
+            petRenderer.transform.localScale = new Vector3(0.4f, 0.4f, 1f);
         }
         else if (cleanliness < 50)
         {
             petRenderer.sprite = dirtySprite;
+            petRenderer.transform.localScale = new Vector3(0.4f, 0.4f, 1f);
         }
         else
         {
             petRenderer.sprite = normalSprite;
+            petRenderer.transform.localScale = new Vector3(0.48f, 0.48f, 1f);
         }
+    }
+
+    private IEnumerator ShowEmotionForSeconds(Sprite emotionSprite)
+    {
+        showingTemporaryEmotion = true;
+
+        petRenderer.sprite = emotionSprite;
+        petRenderer.transform.localScale = new Vector3(0.48f, 0.48f, 1f);
+
+        yield return new WaitForSeconds(1f);
+
+        showingTemporaryEmotion = false;
+        UpdatePetEmotion();
     }
 }
